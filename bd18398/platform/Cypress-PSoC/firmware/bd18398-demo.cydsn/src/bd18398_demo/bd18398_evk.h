RohmSemiconductor/MCU-C-Example-Drivers

commit a2621d6898e781b9604b3a9f72c098373123d5d5

bd18398-demo: Fix board LED usage
- Update logic so that
  Any error => error LED slow blinking - except if:
	OPEN error => error LED fast blinking - except if:
		SHORT error => error LED constantly burning.

- Return old LED state if LED is reserved when blinking is configured
  with *_if_free() function.
- Correctly handle the error LED polarity.

Signed-off-by: Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>

bd18398/platform/Cypress-PSoC/firmware/bd18398-demo.cydsn/src/bd18398_demo/led_demo_led.c
@@ -280,6 +280,8 @@ static void LED_user_handler_short(struct bd18398_led *_this
 				   __attribute__((unused)), void *opaque
 				   __attribute__((unused)))
 {
+	static int ctr[BD18398_NUM_LEDS] = {0};
+
 	/*
 	 * You can enable these prints just to test how the opaque ptr can be
 	 * used. That frequent printing is likely to render the demo pretty
@@ -289,16 +291,32 @@ static void LED_user_handler_short(struct bd18398_led *_this
 	 * printf("User SHORT handler for led %d\r\n", _this->id);
 	 * printf("Extra message: %s\r\n", (char *)opaque);
 	 */
-	/* The SHORT error should have precedence. Blink fast unconditionally */
-	led_demo_err_led_blink(true);
+	if (!(ctr[_this->id] % 1000))
+		printf("User SHORT handler for led %d\r\n", _this->id);
+
+	ctr[_this->id]++;
+	/* The SHORT error should have precedence. Turn ON unconditionally */
+	led_demo_err_led_on();
 }
 
 static void LED_user_handler_open(struct bd18398_led *_this
 				  __attribute__((unused)), void *opaque
 				  __attribute__((unused)))
 {
-	led_demo_err_led_blink_if_free(false);
-	/* printf("User OPEN handler for led %d\r\n", _this->id); */
+	static int ctr[BD18398_NUM_LEDS] = {0};
+	int old_state;
+
+	if (!(ctr[_this->id] % 1000))
+		printf("User OPEN handler for led %d\r\n", _this->id);
+	ctr[_this->id]++;
+
+	/*
+	 * OPEN error should start blinking FAST. Don't blink if we are
+	 * indicating SHORT by constantly burining LED
+	 */
+	old_state = led_demo_err_led_blink_if_free(true);
+	if (old_state != ERR_LED_ON && old_state != ERR_LED_BLINK_FAST)
+		led_demo_err_led_blink(true);
 }
 
 /*

bd18398/platform/Cypress-PSoC/firmware/bd18398-demo.cydsn/bd18398-demo.cywrk.mvaittinen
@@ -13,7 +13,7 @@
 <v>Output</v>
 </TreeKeys>
 <TreeStates>
-<CyGuid_2a325b3b-c96a-4e9f-b7a0-2c44ecb5f237 type_name="CyDesigner.Common.Base.Controls.CyTreeViewState" version="1" SelectedNodeId="bd18398-demo\bd18398-demo">
+<CyGuid_2a325b3b-c96a-4e9f-b7a0-2c44ecb5f237 type_name="CyDesigner.Common.Base.Controls.CyTreeViewState" version="1" SelectedNodeId="bd18398-demo\bd18398-demo\Source Files\bd18398_demo\led_demo_board_leds.c">
 <ExpandedNodeIds>
 <v>bd18398-demo</v>
 <v>bd18398-demo\bd18398-demo</v>
@@ -157,6 +157,89 @@
 <ExpandedNodeIds>
 <v>bd18398-demo</v>
 <v>bd18398-demo\bd18398-demo</v>
+<v>bd18398-demo\bd18398-demo\CortexM3</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\bd18398_evk.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\bd18398_faults.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\bd18398_led.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\CLK_PWM.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\Cm3Start.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\cy_em_eeprom.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\CyBootAsmGnu.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\CyDmac.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\cyfitter_cfg.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\CyFlash.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\CyLib.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\cymetadata.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\cyPm.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\CySpc.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\cyutils.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\gpio_adapter.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\GPIO_P0_1.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\GPIO_P1_0.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\GPIO_P1_1.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\GPIO_P12_2.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\GPIO_P15_0.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\GPIO_P15_1.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\GPIO_P2_0.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\GPIO_P3_0.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\GPIO_P3_2.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\gpio_spec.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\ISR_GPIO_P0_1.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\ISR_GPIO_P1_1.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\ISR_GPIO_P12_2.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\ISR_GPIO_P2_0.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\ISR_GPIO_P3_0.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\led_demo_board_leds.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\led_demo_button.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\led_demo_led.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\led_demo_spi.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\led_demo_time.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\led_ic_usage_example.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\main.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\PWM_0.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\PWM_0_PM.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\PWM_1.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\PWM_1_PM.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\PWM_2.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\PWM_2_PM.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\pwm_adapter.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\REG_PWM_0.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\REG_PWM_0_PM.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\REG_PWM_1.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\REG_PWM_1_PM.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\REG_PWM_2.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\REG_PWM_2_PM.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\spi_driver.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\SPIM.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\SPIM_CLK.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\SPIM_INT.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\SPIM_PM.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\syscall.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\TIMER_0.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\TIMER_0_PM.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\TIMER_0_RST.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\TIMER_0_RST_PM.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\TIMER_1.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\TIMER_1_PM.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\TIMER_1_RST.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\TIMER_1_RST_PM.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\TIMER_2.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\TIMER_2_PM.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\TIMER_2_RST.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\TIMER_2_RST_PM.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\TIMER_CLK.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\UART.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\UART_BOOT.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\UART_INT.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\UART_IntClock.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\UART_PM.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\Listing Files\util.lst</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\bd18398-demo.elf</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\bd18398-demo.hex</v>
+<v>bd18398-demo\bd18398-demo\CortexM3\ARM_GCC_541\Debug\bd18398-demo.map</v>
 <v>bd18398-demo\bd18398-demo\bd18398-demo.gpdsc</v>
 <v>bd18398-demo\bd18398-demo\bd18398-demo.rpt</v>
 <v>bd18398-demo\bd18398-demo\bd18398-demo_timing.html</v>
@@ -164,9 +247,6 @@
 <VisibleNodeIds>
 <v>bd18398-demo</v>
 <v>bd18398-demo\bd18398-demo</v>
-<v>bd18398-demo\bd18398-demo\bd18398-demo.gpdsc</v>
-<v>bd18398-demo\bd18398-demo\bd18398-demo.rpt</v>
-<v>bd18398-demo\bd18398-demo\bd18398-demo_timing.html</v>
 </VisibleNodeIds>
 </CyGuid_2a325b3b-c96a-4e9f-b7a0-2c44ecb5f237>
 </TreeStates>

bd18398/platform/Cypress-PSoC/firmware/bd18398-demo.cydsn/src/bd18398_demo/led_demo_board_leds.c
@@ -56,30 +56,22 @@ static void led_demo_set_board_led(int num, bool state)
 			       EVKIT_GPIO_PIN_DRIVELOW,});
 }
 
-enum board_led_state {
-	ERR_LED_OFF,
-	ERR_LED_ON,
-	ERR_LED_BLINK_SLOW_STARTING,
-	ERR_LED_BLINK_SLOW,
-	ERR_LED_BLINK_FAST_STARTING,
-	ERR_LED_BLINK_FAST,
-};
-
 struct board_led {
 	enum board_led_state state;
 	uint32_t last_access;
 	bool lit;
+	bool inverted;
 	int nmbr;
 };
 
 static void shut_board_led(struct board_led *l)
 {
-	led_demo_set_board_led(l->nmbr, false);
+	led_demo_set_board_led(l->nmbr, l->inverted);
 }
 
 static void lit_board_led(struct board_led *l)
 {
-	led_demo_set_board_led(l->nmbr, true);
+	led_demo_set_board_led(l->nmbr, !l->inverted);
 }
 
 static void brd_led_toggle(struct board_led *l)
@@ -112,8 +104,8 @@ static void led_demo_board_led_blink(struct board_led *el, bool fast)
 	}
 }
 
-static struct board_led g_err_led = { 0, 0, 0,.nmbr = LED_DEMO_ERR_LED };
-static struct board_led g_ind_led = { 0, 0, 0,.nmbr = LED_DEMO_IND_LED };
+static struct board_led g_err_led = { 0, 0, 0, .inverted = true, .nmbr = LED_DEMO_ERR_LED };
+static struct board_led g_ind_led = { 0, 0, 0, .inverted = false, .nmbr = LED_DEMO_IND_LED };
 
 void led_demo_err_led_blink(bool fast)
 {
@@ -122,8 +114,8 @@ void led_demo_err_led_blink(bool fast)
 
 int led_demo_err_led_blink_if_free(bool fast)
 {
-	if (g_err_led.state != ERR_LED_OFF && g_err_led.state != ERR_LED_ON)
-		return -EBUSY;
+	if (g_err_led.state != ERR_LED_OFF)
+		return g_err_led.state;
 
 	led_demo_board_led_blink(&g_err_led, fast);
 
@@ -163,7 +155,7 @@ void led_demo_err_led_off(void)
 	 * The ERR Led control logic is inverted. 
 	 * When line is HIGH, ERR LED is OFF
 	 */
-	led_demo_board_led_on(&g_err_led);
+	led_demo_board_led_off(&g_err_led);
 }
 
 void led_demo_err_led_on(void)
@@ -172,7 +164,7 @@ void led_demo_err_led_on(void)
 	 * The ERR Led control logic is inverted. 
 	 * When line is HIGH, ERR LED is OFF
 	 */
-	led_demo_board_led_off(&g_err_led);
+	led_demo_board_led_on(&g_err_led);
 }
 
 void led_demo_ind_led_off(void)

bd18398/platform/Cypress-PSoC/firmware/bd18398-demo.cydsn/src/bd18398_demo/led_demo_board_leds.h
@@ -35,6 +35,15 @@
 #define SLOW_BLINK_US 1000000
 #define FAST_BLINK_US 100000
 
+enum board_led_state {
+	ERR_LED_OFF,
+	ERR_LED_ON,
+	ERR_LED_BLINK_SLOW_STARTING,
+	ERR_LED_BLINK_SLOW,
+	ERR_LED_BLINK_FAST_STARTING,
+	ERR_LED_BLINK_FAST,
+};
+
 void led_demo_err_led_blink(bool fast);
 void led_demo_err_led_off(void);
 void led_demo_err_led_on(void);

commit f882969d53d0ce919776767d25bc1345e2940ed4

bd18398: Clear protection LED when error condition is cleared
The detection of SHORT error toggles protection LED 'constantly on'
as was designed. When SHORT error is cleared at IC - the LED status
is not changed.

Cache LED error statuses and check if the errors were cleared. Free
the protection LED to indicate other errors (or no errors) if SHORT
is cleared - or if SHORT is not active and OPEN was cleared.

Fixes: a2621d6898e7 ("bd18398-demo: Fix board LED usage")
Reported-by: Thady Bruton <thady.bruton@de.rohmeurope.com>
Signed-off-by: Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>

bd18398/platform/Cypress-PSoC/firmware/bd18398-demo.cydsn/src/bd18398_demo/bd18398_evk.h
@@ -28,6 +28,14 @@
 #ifndef BD19298_EVK_H
 #define BD19298_EVK_H
 
+/*
+ * Defining the DBG_PRINT_LED_ERR_STATUS_CHANGES will cause
+ * print to be emitted when change at LED channel error
+ * status bits is noticed.
+ */
+
+/* #define DBG_PRINT_LED_ERR_STATUS_CHANGES */
+
 int bd18398_evk_config();
 
 #endif

bd18398/platform/Cypress-PSoC/firmware/bd18398-demo.cydsn/src/bd18398_demo/bd18398_led.c
@@ -42,12 +42,12 @@
 #include "led_demo_spi.h"
 #include "bd18398_led.h"
 
-#define RETURN_IF_LED_NOT_SANE(__this) {                \
-	if (!(__this) || (__this)->id > LED_MAX_INDEX) {    \
-        printf("%s: Bad LED ID\r\n", __func__);         \
-                                                        \
-		return -EINVAL;                                 \
-    }                                                   \
+#define RETURN_IF_LED_NOT_SANE(__this) {			\
+	if (!(__this) || (__this)->id > LED_MAX_INDEX) {	\
+		printf("%s: Bad LED ID\r\n", __func__);		\
+								\
+	return -EINVAL;						\
+	}							\
 }
 /* The BD18398 EVK has 3 LED channels */
 static struct bd18398_led __evk_leds[3] = {
@@ -250,6 +250,9 @@ static int led_set_brightness(struct bd18398_led *_this, uint16_t brightness)
 	/* Sanity checks */
 	RETURN_IF_LED_NOT_SANE(_this);
 
+	if (!_this->cached_enabled)
+		return 0;
+
 	if (brightness > BRIGHTNESS_MAX) {
 		printf("%s: Invalid brightness 0x%hx\r\n", __func__,
 		       brightness);
@@ -271,42 +274,49 @@ static uint16_t led_get_brightness(struct bd18398_led *_this)
 	return _this->cached_brightness;
 }
 
-static int led_fault(struct bd18398_led *_this)
+static int led_fault(struct bd18398_led *_this, uint8_t status)
 {
 	/* Sanity checks */
 	RETURN_IF_LED_NOT_SANE(_this);
 
+	_this->fault = status;
+
 	/*
-	 * This is not the correct thing-to-do. We should probably disable
-	 * failing LED but the demo env gives OPEN/SHORT errors all the time.
-	 * Thus we leave the LED ON.
+	 * This does not fell like the correct thing-to-do. We should probably
+	 * disable failing LED channel but the demo env gives OPEN errors all
+	 * the time. We do also want to show the protection errors - and if we
+	 * turn OFF the LED we're likely to mitigate the error. Thus we
+	 * intentionally leave the LED ON in this demo code.
 	 */
+	/* return _this->off(_this); */
+
 	return 0;
-
-	if (_this->fault)
-		return 0;
-
-	_this->fault = true;
-
-	return _this->off(_this);
 }
 
-static bool led_is_faulty(struct bd18398_led *_this)
+/* Return the fault flags (zero if no faults) or negative number on error */
+static int led_is_faulty(struct bd18398_led *_this)
 {
 	RETURN_IF_LED_NOT_SANE(_this);
 
 	return _this->fault;
 }
 
+/*
+ * Clear cached fault flag.
+ * Return the previous fault flags or negative number on error.
+ */
 static int led_fault_cancel(struct bd18398_led *_this)
 {
+	uint8_t ret;
+
 	RETURN_IF_LED_NOT_SANE(_this);
 	if (!_this->fault)
 		return 0;
 
-	_this->fault = false;
+	ret = _this->fault;
+	_this->fault = 0;
 
-	return 0;
+	return ret;
 }
 
 static uint16_t compute_iset(unsigned int mA)
@@ -425,7 +435,7 @@ static int _led_init(struct bd18398_led *_this)
 	_this->fault_cancel = led_fault_cancel;
 	_this->is_enabled = led_is_enabled;
 	_this->handle_led_ocp = _this->handle_sw_ocp = _this->handle_lod =
-	    _this->handle_lsd = NULL;
+		_this->handle_lsd = NULL;
 	_this->dimm_on = led_dimm_on;
 	_this->dimm_off = led_dimm_off;
 	_this->initialized = true;
@@ -544,7 +554,7 @@ static void led_err(struct bd18398_led *led)
 		}
 	}
 	/* Fault (and turn OFF) LED which indicates error */
-	led->set_faulty(led);
+	led->set_faulty(led, ledstatus);
 /*	The demo env I have sets short/open error bits all the time.
  *	Can't turn off the LED for demo.
  *	led->off(led);

bd18398/platform/Cypress-PSoC/firmware/bd18398-demo.cydsn/src/bd18398_demo/bd18398_led.h
@@ -32,6 +32,10 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include "bd18398.h"
+
+#define LED_OPEN(status) ((status) & BIT(BD18398_LED_ERR_BIT_LOD))
+#define LED_SHORT(status) ((status) & BIT(BD18398_LED_ERR_BIT_LSD))
 
 struct bd18398_led;
 
@@ -69,10 +73,10 @@ struct bd18398_led {
 	void *sw_ocp_opaq;
 	void (*handle_led_ocp)(struct bd18398_led * led, void *opaque);	///< led over-current
 	void *led_ocp_opaq;
-	bool (*is_faulty)(struct bd18398_led * _this);	///< has LED been set faulty
+	int (*is_faulty)(struct bd18398_led * _this);	///< has LED been set faulty
 
 	/* TODO: Change these internal? */
-	int (*set_faulty)(struct bd18398_led * _this);	///< set LED as faulty.
+	int (*set_faulty)(struct bd18398_led * _this, uint8_t status);	///< set LED as faulty.
 	int (*fault_cancel)(struct bd18398_led * _this);	///< Cancel LED fault state
 };
 

bd18398/platform/Cypress-PSoC/firmware/bd18398-demo.cydsn/src/bd18398_demo/led_demo_led.c
@@ -77,10 +77,18 @@ struct led_demo_led {
 
 static struct led_demo_led g_leds[BD18398_NUM_LEDS] = { {0, 0, 0, 0, 0} };
 
+/*
+ * This gets called when a non LED error is detected. In this demo
+ * environment this is a nop.
+ *
+ * The LED channel specific errors are indicated directly using
+ * the bd18398_led (led_hw in this file) - layer's 'led error handler' which
+ * parses the status registers for LED channels.
+ */
 static int evk_led_fault(int led_no)
 {
 	struct led_demo_led *l = &g_leds[led_no];
-	struct bd18398_led *led_hw;
+	struct bd18398_led *led_hw __attribute__((unused));
 
 	if (led_no > LED_MAX_INDEX || !l->led_hw)
 		return -EINVAL;
@@ -88,18 +96,20 @@ static int evk_led_fault(int led_no)
 	led_hw = l->led_hw;
 
 	/*
-	 * This is not the correct thing-to-do. We should disable
-	 * failing LED but the demo env gives OPEN/SHORT errors all the time.
+	 * This is not feel like the correct thing-to-do. We should probably disable
+	 * failing LED. Well, we may want to demonstrate how HW copes with the LED
+	 * errors - which is kind of hard if we turn off the LEDs.
 	 * Thus we leave the LED ON.
 	 */
 	return 0;
-
+	/*
 	if (led_hw->is_faulty(led_hw))
 		return 0;
 
-	led_hw->set_faulty(led_hw);
+	led_hw->set_faulty(led_hw, 0xff);
 
 	return led_hw->off(led_hw);
+	*/
 }
 
 static int evk_led_fault_cancel(int led_no)
@@ -109,23 +119,30 @@ static int evk_led_fault_cancel(int led_no)
 	int ret;
 
 	if (led_no > LED_MAX_INDEX || !l->led_hw)
-		return -EINVAL;
+		return 0;
 
 	led_hw = l->led_hw;
 
-	if (!led_hw->is_faulty(led_hw))
-		return 0;
-
 	ret = led_hw->fault_cancel(led_hw);
-	if (ret)
-		return ret;
+	if (ret < 0)
+		return 0;
+	/*
+	 * The EVK DS2 has problem which causes OPEN error to be detected
+	 * all the time. Thus we do not shut down LEDs on errors and we don't
+	 * need to enable them here.
+	 *
+	 * Furthermore, the fault cancel is now called unconditionally prior
+	 * status reading so this would cause LEDs to just flicker.
+	 */
+	/* return led_hw->on(led_hw); */
 
-	return led_hw->on(led_hw);
+	return ret;
 }
 
-static inline void set_all_leds_fault(bool fault)
+/* Set state of all EVK LEDs */
+static inline int set_all_leds_fault(uint8_t *s, int num, bool fault)
 {
-	int i;
+	int i, tmp;
 	int (*f)(int);
 
 	if (fault)
@@ -133,11 +150,17 @@ static inline void set_all_leds_fault(bool fault)
 	else
 		f = evk_led_fault_cancel;
 
-	for (i = 0; i < BD18398_NUM_LEDS; i++)
-		f(i);
+	for (i = 0; i < BD18398_NUM_LEDS; i++) {
+		tmp = f(i);
+		if (tmp < 0)
+			return tmp;
+		if (num > i)
+			s[i] = tmp;
+	}
+
+	return 0;
 }
 
-/* Set state of all EVK LEDs */
 bool evk_any_led_faulty(void)
 {
 	int i;
@@ -150,14 +173,31 @@ bool evk_any_led_faulty(void)
 	return false;
 }
 
-void evk_fault_cancel_all_leds()
+int evk_fault_cancel_all_leds(uint8_t *s, int num)
 {
-	set_all_leds_fault(false);
+	return set_all_leds_fault(s, num, false);
+}
+
+int evk_get_led_errors(uint8_t *s, int num)
+{
+	int i;
+	uint8_t led_faults;
+
+	for (i = 0; i < BD18398_NUM_LEDS; i++)
+		if (g_leds[i].led_hw) {
+			led_faults = g_leds[i].led_hw->is_faulty(g_leds[i].led_hw);
+			if (led_faults < 0)
+				return led_faults;
+			if (i < num)
+				s[i] = led_faults;
+		}
+
+	return 0;
 }
 
 void evk_fault_all_leds()
 {
-	set_all_leds_fault(true);
+	set_all_leds_fault(NULL, 0, true);
 }
 
 /* Set simple 'LED patterns' */
@@ -180,10 +220,16 @@ int evk_led_set_state(int led_no, uint8_t new_pattern, uint8_t new_timing)
 		return -EINVAL;
 	}
 
+	/*
+	 * With the specific EVKs we do always have OPEN error active. So
+	 * we allow setting states even when faults are indicated
+	 */
+	/*
 	if (led_hw->is_faulty(led_hw)) {
 		printf("set_led_state: Led marked faulty - bailing out\r\n");
 		return 0;
 	}
+	*/
 
 	l->led_flags &= (~LED_TIMING_MASK);
 	l->led_flags |= (new_timing & LED_TIMING_MASK);
@@ -239,12 +285,16 @@ void evk_led_loop()
 
 		if (!led_hw)
 			continue;
-
+	/*
+	 * With the specific EVKs we do always have OPEN error active. So
+	 * we allow setting states even when faults are indicated
+	 */
+	/*
 		if (led_hw->is_faulty(led_hw)) {
 			printf("LED %d faulty - skipping handling\r\n", i);
 			continue;
 		}
-
+	*/
 		timing = (l->led_flags & LED_TIMING_MASK);
 		pattern = (l->led_flags & LED_PATTERN_MASK);
 		switch (pattern) {
@@ -291,7 +341,7 @@ static void LED_user_handler_short(struct bd18398_led *_this
 	 * printf("User SHORT handler for led %d\r\n", _this->id);
 	 * printf("Extra message: %s\r\n", (char *)opaque);
 	 */
-	if (!(ctr[_this->id] % 1000))
+	if (!(ctr[_this->id] % 10000))
 		printf("User SHORT handler for led %d\r\n", _this->id);
 
 	ctr[_this->id]++;
@@ -306,7 +356,7 @@ static void LED_user_handler_open(struct bd18398_led *_this
 	static int ctr[BD18398_NUM_LEDS] = {0};
 	int old_state;
 
-	if (!(ctr[_this->id] % 1000))
+	if (!(ctr[_this->id] % 10000))
 		printf("User OPEN handler for led %d\r\n", _this->id);
 	ctr[_this->id]++;
 

bd18398/platform/Cypress-PSoC/firmware/bd18398-demo.cydsn/src/bd18398_demo/led_demo_led.h
@@ -34,13 +34,14 @@
 /* Query LED fault state (cached) */
 bool evk_any_led_faulty(void);
 
-/* Mark LED(s) faulty and turn em OFF */
+/* Mark LED(s) faulty */
 void evk_fault_all_leds(void);
 
 /* Clear faulty mark and re-enable */
-void evk_fault_cancel_all_leds(void);
+int evk_fault_cancel_all_leds(uint8_t *status, int num_status);
 int evk_initialize_leds(void);
 void evk_led_loop(void);
+int evk_get_led_errors(uint8_t *status, int num_status);
 
 /* Set LED pattern and timing */
 /* LED patterns */

bd18398/platform/Cypress-PSoC/firmware/bd18398-demo.cydsn/src/bd18398_demo/led_ic_usage_example.c
@@ -84,7 +84,6 @@ int led_demo_set_config()
 static void led_demo_next_state(int *state)
 {
 	*state = ((*state + 1) % STANDALONE_STATE_LAST);
-	printf("button push detected - state %u\r\n", *state);
 }
 
 /* Functions for state specific actions. */
@@ -170,16 +169,6 @@ static void led_demo_set_state5(void)
 	led_demo_err_led_off();
 }
 
-/* Function to cleat all cahced falts from LEDs */
-bool clear_faults()
-{
-	if (!evk_any_led_faulty())
-		return false;
-
-	evk_fault_cancel_all_leds();
-	return true;
-}
-
 extern void tiktak(void);
 extern void led_demo_board_led_loop(void);
 
@@ -212,8 +201,49 @@ void led_demo_loop(void)
 		 * Please note, the error handling is not re-entrant or
 		 * thread-safe
 		 */
+
 		if (is_failure_detected(&status)) {
+			uint8_t cleared_led_errors[3] = {0};
+			uint8_t active_led_errors[3] = {0};
+			uint8_t clr_err_stack;
+			uint8_t act_err_stack;
+
+			/* We don't turn OFF led at fault. Thus we can just
+			 * zero all faults here and allow the remaining faults
+			 * to be triggered by following status polling.
+			 * This keeps cached fault status Ok and we can toggle
+			 * board LED state (orange) according to the active
+			 * errors.
+			 */
+			evk_fault_cancel_all_leds(&cleared_led_errors[0], 3);
 			handle_errors(status);
+			evk_get_led_errors(&active_led_errors[0], 3);
+
+			clr_err_stack = (cleared_led_errors[0] | cleared_led_errors[1] | cleared_led_errors[2]);
+			act_err_stack = (active_led_errors[0] | active_led_errors[1] | active_led_errors[2]);
+
+#ifdef DBG_PRINT_LED_ERR_STATUS_CHANGES
+			if (cleared_led_errors[0] != active_led_errors[0] || cleared_led_errors[1] != active_led_errors[1] || cleared_led_errors[2] != active_led_errors[2])
+				printf("LED error status change - prev 0x%x 0x%x 0x%x now 0x%x 0x%x 0x%x\r\n", cleared_led_errors[0], cleared_led_errors[1], cleared_led_errors[2], active_led_errors[0], active_led_errors[1], active_led_errors[2]);
+#endif
+			/*
+			 * Handle clearing the error LED state if some errors
+			 * have vanished. If we have SHORT active - we will
+			 * leave LED on. (clear nothing).
+			 * If we don't. We check if either SHORT or OPEN were
+			 * cleared. If they have, then we free the LED so that
+			 * at next loop any less prioritized errors will be
+			 * reflected by the LED.
+			 */
+			if (!LED_SHORT(act_err_stack)) {
+				if (LED_SHORT(clr_err_stack))
+					led_demo_err_led_off();
+
+				if (!LED_OPEN(act_err_stack) &&
+				    LED_OPEN(clr_err_stack))
+					led_demo_err_led_off();
+			}
+
 		} else {
 			led_demo_err_led_off();
 			led_demo_ind_led_on();